5f0eb12c600489fb61b25f8d6c576e46df027f33
Yahel Golan <yahelg@post.bgu.ac.il>
2025-02-17 18:23:39 +0200

    fixed small visual issue in new-listing

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -40,27 +40,27 @@ export default async function NewListingPage() {
         <h2 className="text-lg mt-6">Your companies</h2>
         <p className="text-gray-500 text-sm mb-2">Select a company to create a job add for</p>
         <div>
-          <div className="border inline-block rounded-md">
-            {Object.keys(organizationsNames).map(orgId => (
-              <Link
-                href={'/new-listing/' + orgId} key={orgId}
-                className={
-                  "py-2 px-4 flex gap-2 items-center "
-                  + (Object.keys(organizationsNames)[0] === orgId ? '' : 'border-t')
-                }>
-                {organizationsNames[orgId]}
-                <FontAwesomeIcon className="h-4" icon={faArrowRight} />
-              </Link>
-            ))}
-          </div>
+          {organizationsNames.length ? ( 
+            <div className="border inline-block rounded-md">
+              {Object.keys(organizationsNames).map(orgId => (
+                <Link
+                  href={'/new-listing/' + orgId} key={orgId}
+                  className={
+                    "py-2 px-4 flex gap-2 items-center "
+                    + (Object.keys(organizationsNames)[0] === orgId ? '' : 'border-t')
+                  }>
+                  {organizationsNames[orgId]}
+                  <FontAwesomeIcon className="h-4" icon={faArrowRight} />
+                </Link>
+              ))}
+            </div>
+          ): null}
         </div>
-
-        {organizationMemberships.data.length === 0 && (
+        {organizationMemberships.data.length === 0 ? (
           <div className="border border-blue-200 bg-blue-50 p-4 rounded-md">
             No companies found assigned to your user
           </div>
-        )}
-
+        ): null}
         <Link
           className="inline-flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md mt-6"
           href={'/new-company'}>
